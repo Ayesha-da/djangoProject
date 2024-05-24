62d5c288ccd4da87c70ccc152c8a0463f4425406
kevin <ayesha.school@hotmail.com>
2024-05-23 21:03:29 -0400

    add all

---
+++ djangoProject/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from djangoProject.models import User, Product
+
+#admin.site.register(User)
+#admin.site.register(Product)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ['title', 'username', 'name', 'email']
+    list_display = ['id', 'username', 'name']
+    search_fields = ['id']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price']
+    search_fields = ['id']
+    save_as = True
+    fieldsets = (
+        ("Product Info", {
+            'fields': ('seller', 'name', 'description')
+        }),
+        ("Stock Info", {
+            'fields': ('price', 'stock'),
+            'classes': ('collapse',)
+        })
+    )
